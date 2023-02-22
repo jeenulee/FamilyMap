133b3a9b13a35dd14df07840009a7342f9be10e6
Jeenu Lee <jasonee136@yahoo.com>
2023-02-21 17:50:18 -0700

    added result classes

--- src/main/java/Service/LoginService.java
+++ src/main/java/Service/LoginService.java
@@ -2,29 +2,5 @@ package Service;
 
 public class LoginService {
 
-    private String username;
-    private String password;
 
-
-    public LoginService(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/Service/RegisterService.java
+++ src/main/java/Service/RegisterService.java
@@ -2,46 +2,5 @@ package Service;
 
 public class RegisterService {
 
-    private String username;
-    private String password;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String gender;
 
-
-    public RegisterService(String username, String password, String email, String firstName, String lastName, String gender){
-
-        this.username = username;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-
-    }
-
-    public String getUsername(){return username;}
-
-    public void setUsername(String username){this.username = username;}
-
-
-    public String getPassword() {return password;}
-
-    public void setPassword(String password) {this.password = password;}
-
-    public String getEmail() {return email;}
-
-    public void setEmail(String email) {this.email = email;}
-
-    public String getFirstName() {return firstName;}
-
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-
-    public String getLastName() {return lastName;}
-
-    public void setLastName(String lastName) {this.lastName = lastName;}
-
-    public String getGender() {return gender;}
-
-    public void setGender(String gender) {this.gender = gender;}
 }
